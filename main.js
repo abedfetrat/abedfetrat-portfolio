312b305edc0a66c256bc4ed66fb0fa1cd28a33bb
Abed Fetrat <abedfetrat@gmail.com>
2023-08-26 20:14:41 +0200

    add transitions to projects section

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ window.onscroll = () => {
         nav.classList.remove("scrolled");
     }
 };
+
+// Animate projects when scrolled into view
+const projects = [...document.querySelectorAll('.project')];
+projects.forEach(el => addObserver(el, {threshold: 0.2}));
+const moreBtn = document.querySelector('.projects__more');
+addObserver(moreBtn, {threshold: 1.0});
+
+function addObserver(el, opt) {
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-up');
+                observer.unobserve(entry.target);
+            }
+        })
+    }, opt);
+    observer.observe(el);
+}
