508956e435cda66ee964a7332f04d0ddc4aab15b
Abed Fetrat <abedfetrat@gmail.com>
2023-08-28 16:13:49 +0200

    add contact section & minor style tweaks

--- main.js
+++ main.js
@@ -9,20 +9,29 @@ window.onscroll = () => {
     }
 };
 
-// Animate projects when scrolled into view
-const projects = [...document.querySelectorAll('.project')];
-projects.forEach(el => addObserver(el, {threshold: 0.2}));
-const moreBtn = document.querySelector('.projects__more');
-addObserver(moreBtn, {threshold: 1.0});
+// Animate projects and contact section when scrolled into view
+function scrollTrigger() {
+    const projectSectionHeading = document.querySelector('.projects__heading');
+    addObserver(projectSectionHeading, { threshold: 1.0 });
+    const projects = [...document.querySelectorAll('.project')];
+    projects.forEach(el => addObserver(el, { threshold: 0.2 }));
+    const moreBtn = document.querySelector('.projects__more');
+    addObserver(moreBtn, { threshold: 1.0 });
 
-function addObserver(el, opt) {
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('fade-up');
-                observer.unobserve(entry.target);
-            }
-        })
-    }, opt);
-    observer.observe(el);
+    const contactSection = document.querySelector('#contact');
+    addObserver(contactSection, { threshold: 0.6 });
+
+    function addObserver(el, opt) {
+        let observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('fade-up');
+                    observer.unobserve(entry.target);
+                }
+            })
+        }, opt);
+        observer.observe(el);
+    }
 }
+
+scrollTrigger();
