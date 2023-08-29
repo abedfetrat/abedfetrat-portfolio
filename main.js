02ad9ae4234bc91e82b1a1d992f4d9d2623d98c4
Abed Fetrat <abedfetrat@gmail.com>
2023-08-29 11:56:59 +0200

    make navbar functional & improve style

--- main.js
+++ main.js
@@ -9,29 +9,41 @@ window.onscroll = () => {
     }
 };
 
+// Set navbar active link based on which section is in view
+const navLinks = document.querySelector(".primary-nav__links");
+const pageSections = document.querySelectorAll("section");
+pageSections.forEach((el) => addObserver(el, { rootMargin: '-50% 0% -20% 0%' }, (entry) => {
+    if (entry.isIntersecting) {
+        const section = entry.target.id;
+        navLinks.querySelectorAll(".primary-nav__link").forEach(el => el.classList.remove("active"));
+        navLinks.querySelector(`a[href='#${section}']`).classList.add("active");
+    }
+}));
+
 // Animate projects and contact section when scrolled into view
+scrollTrigger();
+
 function scrollTrigger() {
     const projectSectionHeading = document.querySelector('.projects__heading');
-    addObserver(projectSectionHeading, { threshold: 1.0 });
+    addObserver(projectSectionHeading, { threshold: 1.0 }, cb);
     const projects = [...document.querySelectorAll('.project')];
-    projects.forEach(el => addObserver(el, { threshold: 0.2 }));
+    projects.forEach(el => addObserver(el, { threshold: 0.2 }, cb));
     const moreBtn = document.querySelector('.projects__more');
-    addObserver(moreBtn, { threshold: 1.0 });
-
+    addObserver(moreBtn, { threshold: 1.0 }, cb);
     const contactSection = document.querySelector('#contact');
-    addObserver(contactSection, { threshold: 0.6 });
+    addObserver(contactSection, { threshold: 0.6 }, cb);
 
-    function addObserver(el, opt) {
-        let observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('fade-up');
-                    observer.unobserve(entry.target);
-                }
-            })
-        }, opt);
-        observer.observe(el);
+    function cb(entry, observer) {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-up');
+            observer.unobserve(entry.target);
+        }
     }
 }
 
-scrollTrigger();
+function addObserver(el, opt, cb) {
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => cb(entry, observer))
+    }, opt);
+    observer.observe(el);
+}
